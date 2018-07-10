e73b7ff718af8542adcf0c8c4b8d7d67c28b4c1d
Keenan-R-Faulkner <keenanrfaulkner.webaccess@gmail.com>
2018-07-10 11:59:47 -0600

    created content component

---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render() {
+        return (
+            <p>
+
+            </p>    
+        )
+    }
+}
+
+export default Content;
